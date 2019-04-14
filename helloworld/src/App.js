4ca5101d8ccc9181bac28a355ba0fb66d4d873b2
阿部直也 <n.abe@gemcook.com>
2019-04-14 21:09:35 +0900

    [update] why-jsx

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 
+// class App extends Component {
+//   render() {
+//     return <div>Hello Micropig！</div>;
+//   }
+// }
+
 class App extends Component {
   render() {
-    return <p>Hello Micropig！</p>;
+    return <h1>Hello Micropig!</h1>;
   }
 }
 
