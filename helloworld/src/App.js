2317cdf9c7179bd79f58f87209e3da189f2249bf
阿部直也 <n.abe@gemcook.com>
2019-04-14 21:32:41 +0900

    [update] more-jsx_React.Fragment

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,14 +1,26 @@
+//Reactをimportしていないとトランスパイル後のReact.element()が実行できないので、このReactは必要。
+//逆に言うとJSXを使わないとこのReactは不必要
 import React, {Component} from 'react';
-
-// class App extends Component {
-//   render() {
-//     return <div>Hello Micropig！</div>;
-//   }
-// }
 
 class App extends Component {
   render() {
-    return <h1>Hello Micropig!</h1>;
+    return (
+      // Reactでは返すJSXは一つでなければならない
+      // <div> 余計なdivタグが生まれる
+      <React.Fragment>
+        {/* <React.Fragment>を使うことで、余計なdivタグを作らずにJSXを包むことができる */}
+        {/* labelのfor属性：ラジオボタンを置いた時などに、ラジオボタンのラベルにラジオボタンのid属性をfor属性として付与することで、ラベルを押した時でもラジオボタンが反応するようになる */}
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          //onClick：クリック時  onChange:インプットの中身が変わった時
+          onChange={() => {
+            console.log('I love micropig');
+          }}
+        />
+      </React.Fragment>
+      // </div>
+    );
   }
 }
 
