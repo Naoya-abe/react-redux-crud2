6250f6d3f8b2b66cd72c37eb0b3628c5b68f652c
阿部直也 <n.abe@gemcook.com>
2019-04-18 22:42:08 +0900

    [update] components

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,27 +1,33 @@
-//Reactをimportしていないとトランスパイル後のReact.element()が実行できないので、このReactは必要。
-//逆に言うとJSXを使わないとこのReactは不必要
-import React, {Component} from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      // Reactでは返すJSXは一つでなければならない
-      // <div> 余計なdivタグが生まれる
-      <React.Fragment>
-        {/* <React.Fragment>を使うことで、余計なdivタグを作らずにJSXを包むことができる */}
-        {/* labelのfor属性：ラジオボタンを置いた時などに、ラジオボタンのラベルにラジオボタンのid属性をfor属性として付与することで、ラベルを押した時でもラジオボタンが反応するようになる */}
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          //onClick：クリック時  onChange:インプットの中身が変わった時
-          onChange={() => {
-            console.log('I love micropig');
-          }}
-        />
-      </React.Fragment>
-      // </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="pig">pig</label>
+//         <input
+//           type="text"
+//           onChange={() => {
+//             console.log('I love micropig');
+//           }}
+//         />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Pig />
+      <Pig />
+      <Pig />
+    </div>
+  );
+};
+
+const Pig = () => {
+  return <div>oink oink</div>;
+};
 
 export default App;
